2fa6fe6947ac30d1ef3fe9f7c3ecc5ec90ce96ec
longjiezhong <30586750+longjiezhong@users.noreply.github.com>
2017-08-22 18:16:11 +0800

    rr
    rr

--- capter6/ex6.5.4/ex6.5.4/main.cpp
+++ capter6/ex6.5.4/ex6.5.4/main.cpp
@@ -1,8 +1,8 @@
-#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
-
-using namespace std;
 using namespace cv;
+using namespace std;
 
 Mat g_srcImage, g_dstImage, g_grayImage, g_maskImage;
 
@@ -33,18 +33,22 @@ static void onMouse(int event, int x, int y, int, void*)
 	Mat dst = g_bIsColor ? g_dstImage : g_grayImage;
 	int area;
 
-	if(g_bUseMask)
+	if( g_bUseMask )
 	{
-		threshold(g_maskImage, g_maskImage, 1, 128, THRESH_BINARY);
-		area = FloodFill(dst, g_maskImage, seed, newVal, &ccomp,
+		threshold(g_maskImage, g_maskImage, 1, 128, CV_THRESH_BINARY);
+		area = floodFill(dst,				//被处理的图像
+			g_maskImage,					//掩膜；
+			seed, 
+			newVal, 
+			&ccomp, 
 			Scalar(LowDifference, LowDifference, LowDifference),
-			Scalar(UpDifference, UpDifference, UpDifference), flags);
-		imshow("mask", g_maskImage);
+			Scalar(UpDifference, UpDifference, UpDifference), 
+			flags);
+		imshow( "mask", g_maskImage );
 	}
 	else
 	{
-		area = FloodFill(dst, seed, newVal, &ccomp, 
-			Scalar(LowDifference, LowDifference, LowDifference),
+		area = floodFill(dst, seed, newVal, &ccomp, Scalar(LowDifference, LowDifference, LowDifference),
 			Scalar(UpDifference, UpDifference, UpDifference), flags);
 	}
 
@@ -54,16 +58,16 @@ static void onMouse(int event, int x, int y, int, void*)
 
 int main(int argc, char** argv)
 {
-	g_srcImage = imread("1.bmp", 1);
+	g_srcImage = imread("1.png", 1);
 	if( !g_srcImage.data )
 		cout << "读取图片错误！" << endl;
 	g_srcImage.copyTo(g_dstImage);
 	cvtColor(g_srcImage, g_grayImage, COLOR_RGB2GRAY);
-	g_maskImage.create(g_srcImage.row+2, g_srcImage.cols+2, CV_8UC1);
+	g_maskImage.create(g_srcImage.rows+2, g_srcImage.cols+2, CV_8UC1);
 	namedWindow("效果图", CV_WINDOW_AUTOSIZE);
 
-	createTrackbar("负差最大值"， "效果图", &g_nLowDifference, 255, 0);
-	createTrackbar("正差最大值"， "效果图", &g_nUpDifference, 255, 0);
+	createTrackbar("负差最大值", "效果图", &g_nLowDifference, 255, 0);
+	createTrackbar("正差最大值", "效果图", &g_nUpDifference, 255, 0);
 
 	setMouseCallback("效果图", onMouse, 0);
 
@@ -80,20 +84,70 @@ int main(int argc, char** argv)
 		}
 
 		switch( c )
-			case '1':
-				if( g_bIsColor )
-				{
-					//cout << "键盘“1”被按下，切换彩色/灰度模式" << endl;
-					cvtColor(g_srcImage, g_grayImage, COLOR_RGB2GRAY);
-					g_maskImage = Scalar::all(0);
-					g_bIsColor = false;
-				}
-				else
-				{
-					g_srcImage.copyTo(g_dstImage);
-					g_maskImage = Scalar::all(0);
-					g_bIsColor = true;
-				}
-				break;
+		{
+		case '1':
+			if( g_bIsColor )
+			{
+				//cout << "键盘“1”被按下，切换彩色/灰度模式" << endl;
+				cvtColor(g_srcImage, g_grayImage, COLOR_RGB2GRAY);
+				g_maskImage = Scalar::all(0);
+				g_bIsColor = false;
+			}
+			else
+			{
+				g_srcImage.copyTo(g_dstImage);
+				g_maskImage = Scalar::all(0);
+				g_bIsColor = true;
+			}
+			break;
+
+		case '2' :
+			if(g_bUseMask)
+			{
+				destroyWindow("mask");
+				g_bUseMask = false;
+			}
+			else
+			{
+				namedWindow("mask", 0);
+				g_maskImage = Scalar::all(0);
+				imshow("mask", g_maskImage);
+				g_bUseMask = true;
+			}
+			break;
+
+		case '3':
+			cout << "按键3被按下，恢复原始图像" << endl;
+			g_srcImage.copyTo(g_dstImage);
+			cvtColor(g_dstImage, g_grayImage, COLOR_RGB2GRAY);
+			g_maskImage = Scalar::all(0);
+			break;
+
+		case '4':
+			cout << "按键4被按下，使用空范围的漫水填充" << endl;
+			g_nFillMode = 0;
+			break;
+
+		case '5':
+			cout << "按键5被按下，使用渐变、固定范围的漫水填充" << endl;
+			g_nFillMode = 1;
+			break;
+
+		case '6':
+			cout << "按键6被按下，使用渐变、浮动范围的漫水填充" << endl;
+			g_nFillMode = 2;
+			break;
+
+		case '7':
+			cout << "按键7被按下，操作标志位的低8位使用4位的连接模式" << endl;
+			g_nConnectivity = 4;
+			break;
+
+		case '8':
+			cout << "按键8被按下，操作标志位的低8位使用8位的链接模式" << endl;
+			g_nConnectivity  = 8;
+			break;
+		}
 	}
+	return 0;
 }
