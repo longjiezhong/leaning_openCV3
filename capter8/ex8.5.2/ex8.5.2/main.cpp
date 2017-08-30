98dfb976840dad1b70f08167af2e6a8c278752b1
longjiezhong <30586750+longjiezhong@users.noreply.github.com>
2017-08-30 23:14:28 +0800

    8

--- capter8/ex8.5.2/ex8.5.2/main.cpp
+++ capter8/ex8.5.2/ex8.5.2/main.cpp
@@ -9,11 +9,11 @@ using namespace cv;
 Mat g_maskImage, g_srcImage;
 Point prevPt(-1, -1);
 
-static void on_Mouse(int, event, int x, int y, int flags, void*);
+static void on_Mouse(int event, int x, int y, int flags, void*);
 
 int main()
 {
-	g_srcImage = imread("1.jpg", 1);
+	g_srcImage = imread("gray.bmp", 1);
 	imshow(WINDOW_NAME, g_srcImage);
 	Mat srcImage, grayImage;
 	g_srcImage.copyTo(srcImage);
@@ -30,6 +30,92 @@ int main()
 		if(c == 27)
 			break;
 
-		if()
+		if( c == '2')
+		{
+			g_maskImage = Scalar::all(0);
+			srcImage.copyTo(g_srcImage);
+			imshow("image", g_srcImage);
+		}
+
+		if( c == ' ')
+		{
+			int i, j, compCount = 0;
+			vector<vector<Point>> contours;
+			vector<Vec4i> hierarchy;
+
+			findContours(g_maskImage, contours, hierarchy, CV_RETR_CCOMP,
+				CV_CHAIN_APPROX_SIMPLE);
+
+			if(contours.empty())
+				continue;
+
+			Mat maskImage(g_maskImage.size(), CV_32S);
+			maskImage = Scalar::all(0);
+
+			for(int index = 0; index >= 0; index = hierarchy[index][0],
+				compCount++)
+				drawContours(maskImage, contours, index,
+				Scalar::all(compCount+1), -1, 8, hierarchy, INT_MAX);
+
+			if(compCount == 0)
+				continue;
+
+			vector<Vec3b> colorTab;
+			for(int i = 0; i < compCount; i++)
+			{
+				int b = theRNG().uniform(0, 255);
+				int g = theRNG().uniform(0, 255);
+				int r = theRNG().uniform(0, 255);
+
+				colorTab.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+			}
+
+			watershed(srcImage, maskImage);
+
+			Mat watershedImage(maskImage.size(), CV_8UC3);
+			for(i = 0; i < maskImage.rows; i++)
+			{
+				for(j = 0; j <maskImage.cols; j++)
+				{
+					int index = maskImage.at<int>(i,j);
+					if(index == -1)
+						watershedImage.at<Vec3b>(i, j) = Vec3b(255, 255, 255);
+
+					else if(index <= 0 || index > compCount)
+						watershedImage.at<Vec3b>(i, j) = Vec3b(0, 0, 0);
+
+					else
+						watershedImage.at<Vec3b>(i, j) = colorTab[index - 1];
+				}
+			}
+
+			watershedImage = watershedImage * 0.5 + grayImage * 0.5;
+			imshow("watershed transform", watershedImage);
+		}
+	}
+
+	return 0;
+}
+
+static void on_Mouse(int event, int x, int y, int flags, void*)
+{
+	if(x < 0 || x >= g_srcImage.cols || y < 0 || y >= g_srcImage.rows)
+		return;
+
+	if(event == CV_EVENT_LBUTTONUP || !(flags & CV_EVENT_FLAG_LBUTTON))
+		prevPt = Point(-1, -1);
+	else if(event == CV_EVENT_LBUTTONDOWN)
+		prevPt = Point(x, y);
+
+	else if(event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON))
+	{
+		Point pt(x, y);
+		if(prevPt.x < 0)
+			prevPt = pt;
+
+		line(g_maskImage, prevPt, pt, Scalar::all(255), 5, 8, 0);
+		line(g_srcImage, prevPt, pt, Scalar::all(255), 5, 8, 0);
+		prevPt = pt;
+		imshow(WINDOW_NAME, g_srcImage);
 	}
 }
